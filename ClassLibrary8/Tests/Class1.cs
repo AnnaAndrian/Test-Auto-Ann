dc9ef6991ea0aa752cfa74422afb49f57826f136
Ann Test <andrianova.anva@gmail.com>
2019-05-30 16:51:08 +0300

    initial commit

---
+++ ClassLibrary8/Tests/Class1.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassLibrary8.Tests
+{
+    class FirstTEst
+    {
+        [Test]
+        public void FirstTest()
+        {
+           Assert.That(1, Is.EqualTo(1));
+
+        }
+    }
+}
